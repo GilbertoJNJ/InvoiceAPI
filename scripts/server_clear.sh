e49952a63b0aff869b6a17b06d5f211f18100b87
Gilberto Júnior <gilbertojnj@gmail.com>
2024-02-15 19:12:37 -0300

    Limpa o conteudo do diretorio

--- scripts/server_clear.sh
+++ scripts/server_clear.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-rm -rf /home/ec2-user/server
+
+# Limpar o conteúdo do diretório /home/ec2-user/server, exceto o diretório "scripts"
+find /home/ec2-user/server -mindepth 1 -maxdepth 1 ! -name 'scripts' -exec rm -rf {} +
